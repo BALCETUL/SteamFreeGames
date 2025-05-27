798708593ddb83df241c3f4ed8a48dafdf020dcd
BALCETUL <69043192+BALCETUL@users.noreply.github.com>
2025-05-27 03:20:28 +0300

    Update NeedFree.py

--- NeedFree.py
+++ NeedFree.py
@@ -7,20 +7,21 @@ import json
 import pytz
 import bs4
 
+
 API_URL_TEMPLATE = "https://store.steampowered.com/search/results/?query&start={pos}&count=100&infinite=1"
 THREAD_CNT = 8
 
 free_list = queue.Queue()
 
 def fetch_Steam_json_response(url):
-    ''' Получает JSON-ответ от Steam API
-    URL:            URL Steam WebAPI
+    ''' Fetch json response from Steam API
+    URL:            Steam WebAPI url
 
-    return:         JSON-контент
+    return:         json content
     '''
     while True:
         try:
-            with requests.get(url, timeout=5) as response:
+            with requests.get(url, timeout = 5) as response:
                 ret_json = response.json()
             return ret_json
         except Exception as e:
@@ -28,26 +29,27 @@ def fetch_Steam_json_response(url):
             time.sleep(10)
             continue
 
-def get_free_goods(start, append_list=False):
-    ''' Извлекает список товаров со 100% скидкой из 100 продуктов
-    start:          начальный индекс страницы
-    append_list:    добавлять ли найденные бесплатные товары в финальный список
+def get_free_goods(start, append_list = False):
+    ''' Extract 100%-discount goods list in a list of 100 products
+    start:          start page index
+    append_list:    if to append new found free goods to final list
 
-    return:         количество товаров
+    return:         goods_count
     '''
+
     global free_list
     retry_time = 3
 
     while retry_time >= 0:
-        response_json = fetch_Steam_json_response(API_URL_TEMPLATE.format(pos=start))
+        response_json = fetch_Steam_json_response(API_URL_TEMPLATE.format(pos = start))
         try:
             goods_count = response_json["total_count"]
             goods_html = response_json["results_html"]
             page_parser = bs4.BeautifulSoup(goods_html, "html.parser")
-            full_discounts_div = page_parser.find_all(name="div", attrs={"class": "search_discount_block", "data-discount": "100"})
+            full_discounts_div = page_parser.find_all(name = "div", attrs = {"class":"search_discount_block", "data-discount":"100"})
             sub_free_list = [
                 [
-                    div.parent.parent.parent.parent.find(name="span", attrs={"class": "title"}).get_text(),
+                    div.parent.parent.parent.parent.find(name = "span", attrs = {"class":"title"}).get_text(),
                     div.parent.parent.parent.parent.get("href"),
                 ] for div in full_discounts_div
             ]
@@ -58,24 +60,24 @@ def get_free_goods(start, append_list=False):
 
             return goods_count
         except Exception as e:
-            print("get_free_goods: ошибка на start = %d, осталось попыток %d" % (start, retry_time))
+            print("get_free_goods: error on start = %d, remain retry %d time(s)" % (start, retry_time))
             print(e)
             retry_time -= 1
-    print("get_free_goods: ошибка на start = %d, пропускаем" % (start))
+    print("get_free_goods: error on start = %d, throw" % (start))
 
     return 0
 
-# Получаем общее количество товаров
+# Get total count of free goods
 tryget_first_page = get_free_goods(0)
 total_count = tryget_first_page
 
-# Многопоточный парсинг
-threads = ThreadPoolExecutor(max_workers=THREAD_CNT)
+# Multi-thread crawling
+threads = ThreadPoolExecutor(max_workers = THREAD_CNT)
 futures = [threads.submit(get_free_goods, index, True) for index in range(0, total_count, 100)]
 
 wait(futures, return_when=ALL_COMPLETED)
 
-# Обрабатываем список бесплатных игр
+# Process free list
 final_free_list = []
 free_names = set()
 while not free_list.empty():
@@ -89,5 +91,5 @@ with open("free_goods_detail.json", "w") as fp:
     json.dump({
         "total_count": len(final_free_list),
         "free_list": final_free_list,
-        "update_time": datetime.datetime.now(tz=pytz.timezone("Europe/Kiev")).strftime('%Y-%m-%d %H:%M:%S')
+        "update_time": datetime.datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime('%Y-%m-%d %H:%M:%S')
     }, fp)
